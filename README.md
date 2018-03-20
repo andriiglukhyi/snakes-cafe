5819e0d1b6b5484e35006806b65039d98fa4b0aa
austincmatteson <austin.c.matteson@gmail.com>
2018-03-19 18:52:51 -0700

    clean keyboard interrupt and readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Snakes Cafe
+
+**Author**: Andrii Glukhyi & Austin Matteson
+**Version**: 1.0.0 
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for a Code Fellows 401 class. (i.e. What's your problem domain?) -->
+This application is a command line restaurant. It allows the user to place an order from a specified menu. It will count how many times each item was ordered. It allows the user to view their entire order and quit from the program.
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+Clone the repo and run snakes_cafe.py using their python3 environment
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. This is also an area which you can include any visuals; flow charts, example usage gifs, screen captures, etc.-->
+Python3
+
+## API
+<!-- Provide detailed instructions for your applications usage. This should include any methods or endpoints available to the user/client/developer. Each section should be formatted to provide clear syntax for usage, example calls including input data requirements and options, and example responses or return values. -->
+None.
+
+## Change Log
+<!-- Use this are to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Added functionality to add and delete some things.
+-->
+03-19-2018 4:30pm - Created Repo
+03-19-2018 5:00pm - Printed Menu
+03-19-2018 5:30pm - User Input
+03-19-2018 6:00pm - Cleaned Functionality
+03-19-2018 6:30pm - Keyboard interrupt cleanly
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    import sys
     def cafe():
         appitizers = {
             'wings': 0,
@@ -43,19 +44,22 @@ if __name__ == '__main__':
         print('\n' + '*' * 35 + '\n' + '** What would you like to order?**\n' +
               '*' * 35)
         while True:
-            order = input('> ').lower()
-            if (order in appitizers or order in entrees or order in desserts or
-                    order in drinks):
-                final_order[order] = 1 + final_order.get(order, 0)
-                print('\n** ' + str(final_order[order]) + ' order(s) of ' +
-                      str(order) + ' has been added to your meal **\n')
-            elif order.lower() == 'order':
-                print('\n** You have ordered **')
-                for key, val in final_order.items():
-                    print('** {} order(s) {} **'.format(val, key))
-            elif order.lower() == 'quit':
-                break
-            else:
-                print('\n** That item is not on the menu **\n')
+            try:
+                order = input('> ').lower()
+                if (order in appitizers or order in entrees or order in
+                        desserts or order in drinks):
+                    final_order[order] = 1 + final_order.get(order, 0)
+                    print('\n** ' + str(final_order[order]) + ' order(s) of ' +
+                          str(order) + ' has been added to your meal **\n')
+                elif order.lower() == 'order':
+                    print('\n** You have ordered **')
+                    for key, val in final_order.items():
+                        print('** {} order(s) {} **'.format(val, key))
+                elif order.lower() == 'quit':
+                    break
+                else:
+                    print('\n** That item is not on the menu **\n')
+            except KeyboardInterrupt:
+                sys.exit(0)
 
 cafe()
