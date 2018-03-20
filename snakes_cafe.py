6a7691a39aaf302cddea3f6cb91acb542e7f5685
andriiglukhyi <andrigluxii@gmail.com>
2018-03-20 11:28:25 -0700

    new

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,6 +1,6 @@
-if __name__ == '__main__':
-    
-    def cafe():
+    import sys
+
+def cafe():
         appitizers = {
             'wings': 0,
             'cookies': 0,
@@ -43,21 +43,23 @@ if __name__ == '__main__':
 
         print('\n' + '*' * 35 + '\n' + '** What would you like to order?**\n' +
               '*' * 35)
-        while True:
-            order = input('> ').lower()
-            if (order in appitizers or order in entrees or order in
-                    desserts or order in drinks):
-                final_order[order] = 1 + final_order.get(order, 0)
-                print('\n** ' + str(final_order[order]) + ' order(s) of ' +
-                        str(order) + ' has been added to your meal **\n')
-            elif order.lower() == 'order':
-                print('\n** You have ordered **')
-                for key, val in final_order.items():
-                    print('** {} order(s) {} **'.format(val, key))
-            elif order.lower() == 'quit':
-                break
-            else:
-                print('\n** That item is not on the menu **\n')
-                
+        while True
+                order = input('> ').lower()
+                if (order in appitizers or order in entrees or order in
+                        desserts or order in drinks):
+                    final_order[order] = 1 + final_order.get(order, 0)
+                    print('\n** ' + str(final_order[order]) + ' order(s) of ' +
+                          str(order) + ' has been added to your meal **\n')
+                elif order.lower() == 'order':
+                    print('\n** You have ordered **')
+                    for key, val in final_order.items():
+                        print('** {} order(s) {} **'.format(val, key))
+                elif order.lower() == 'quit':
+                    break
+                else:
+                    print('\n** That item is not on the menu **\n')
 
-cafe()
+
+if __name__ == '__main__':
+    cafe()
+    
