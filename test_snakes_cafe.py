82169451a162eb2b8d14d4c5efbc6d58fdd4f31b
andriiglukhyi <andrigluxii@gmail.com>
2018-03-20 20:32:01 -0700

    new

--- test_snakes_cafe.py
+++ test_snakes_cafe.py
@@ -1,33 +1,50 @@
 import snakes_cafe
 
 def test_remove ():
-
+    """
+    Test will test remove function. It the add 1 to the currentt value 
+    """
     snakes_cafe.final_order['tea'] = 5
     snakes_cafe.remove('tea')
     assert snakes_cafe.final_order['tea'] == 4
 
 def test_order_total():
+    """
+    Test will count the total dependence on the current order backet('final order')
+    """
     snakes_cafe.final_order['wings'] = 1
     snakes_cafe.final_order['tea'] = 3
     assert snakes_cafe.order_total() == 19.02
 
 def test_bill():
+    """
+    Test will count the bill dependence on the current order backet('final order')
+    """
     snakes_cafe.final_order['wings'] = 20
     snakes_cafe.final_order['tea'] = 20
     assert snakes_cafe.bill() == 289
 
 
 def test_search():
+    """
+    Test will search for spesick menu item and will print all item inside that menu item ')
+    """
     key = 'entrees'
     assert snakes_cafe.search(key) == ['salmon', 'steak', 'country fried chicken', 'chopped grilled angus', 'crispy fish tacos', 'meat tornado'].sort()
 
 def test_remove_0():
+     """
+    Test will if function will remove one item from current backet')
+    """
     snakes_cafe.final_order['tea'] = 1
     snakes_cafe.remove('tea')
     # import pdb; pdb.set_trace()
     assert ['tea'] not in snakes_cafe.final_order.keys()
 
 def test_add_to_order_one_elemant():
+     """
+    Test will test current order if we add one element two identic item')
+    """
     snakes_cafe.final_order = {}
     snakes_cafe.add_to_order('cake')
     snakes_cafe.add_to_order('cake')
@@ -35,6 +52,9 @@ def test_add_to_order_one_elemant():
 
 
 def test_add_to_order():
+     """
+    Test will check if the current backet will chanh if we add one item to our order  ')
+    """
     snakes_cafe.final_order['wings'] = 10
     snakes_cafe.add_to_order('wings')
     assert snakes_cafe.final_order['wings'] == 11
